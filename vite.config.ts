389cb86a27a7f9218b712901e9742a46070d9a01
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2025-04-22 23:14:53 -0500

    building

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,23 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
+    server: {
+        host: "::",
+        port: 8080,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8083",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+        Boolean
+    ),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 }));
